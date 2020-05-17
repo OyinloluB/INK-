12a702c3c87bc974a647ac58b2eb656460d25084
timzprof <timilehin.olumofin@gmail.com>
2020-05-17 12:25:51 +0100

    Added Cloudinary Upload Feature to the API

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
+const {config} = require('dotenv');
 const connectDB = require("./config/db");
+const cloudinaryUtility = require('./util/cloudinary');
+
+config();
 
 const app = express();
 
 //Connect Database
 connectDB();
 
+//Init Cloudinary
+cloudinaryUtility.init();
+
 //Init middleware
 app.use(express.json({ extended: false }));
+app.use(fileUpload());
 
 app.get("/", (req, res) => res.json({ msg: "Welcome to INK" }));
 
